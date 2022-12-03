4eb0794fb7e355854e25da28e812bbb54cca4ff1
George Maddocks <gmaddocks1@gmail.com>
2022-12-03 23:07:13 +0000

    Rust never sleeps

---
+++ src/days/day1.rs
@@ -0,0 +1,3 @@
+pub fn solution() {
+    println!("Hello world")
+}
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod days;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let day: u8 = args[1]
+        .parse()
+        .unwrap_or_else(|v| panic!("Day is invalid: {}", v));
+
+    call_day(day)
+}
+
+fn call_day(day: u8) {
+    match day {
+        1 => days::day1::solution(),
+        _ => unimplemented!(),
+    }
+}
