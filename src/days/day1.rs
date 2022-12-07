275224d7049817b36fe4611df51e8346a0d4a81e
George Maddocks <gmaddocks1@gmail.com>
2022-12-07 22:54:17 +0000

    First working solution! Day 1 - 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -2,5 +2,24 @@ use crate::helpers::reader::read_file;
 
 pub fn solution() {
     let string = read_file("./src/days/inputs/1".to_owned());
-    println!("{}", string)
+    let string_without_last_char = &string[0..&string.len() - 1];
+    let calories_vec: Vec<Vec<i32>> = string_without_last_char
+        .split("\n\n")
+        .map(|s| {
+            s.split('\n')
+                .map(|num| {
+                    num.parse::<i32>()
+                        .unwrap_or_else(|v| panic!("Num is invalid: {}", v))
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut sums: Vec<i32> = calories_vec
+        .iter()
+        .map(|calories| calories.iter().sum::<i32>())
+        .collect();
+
+    sums.sort_by(|a, b| b.cmp(a));
+    println!("Solution: {:?}", sums[0])
 }
